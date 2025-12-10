f529c2cdbfca8912c79824e28ffb93ea0e328d54
Angel <apenalver4@gmail.com>
2025-12-10 17:36:02 -0400

    chore: setup project infrastructure and docker environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 
 @Global()
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    entities: [],
+    synchronize: true,
+  }),
+  ElasticsearchModule.register({
+    node: process.env.ELASTICSEARCH_NODE,
+  }),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
