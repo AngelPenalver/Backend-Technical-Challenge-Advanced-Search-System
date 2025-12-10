9f257925ebfdd634e0f75706f3021c8bf6858ca2
Angel <apenalver4@gmail.com>
2025-12-10 17:37:49 -0400

    feat(infra): implement product entity and typeorm configuration

---
+++ src/products/infrastructure/persistence/entities/product.mapper.ts
@@ -0,0 +1,28 @@
+import { Product } from "../../../domain/models/product.model";
+import { ProductEntity } from "./product.entity";
+
+export class ProductMapper {
+    static toDomain(entity: ProductEntity): Product {
+        return new Product(
+            entity.id,
+            entity.name,
+            entity.description,
+            Number(entity.price),
+            entity.stock,
+            entity.category,
+            entity.createdAt,
+            entity.updatedAt
+        );
+    }
+
+    static toEntity(domain: Product): ProductEntity {
+        const entity = new ProductEntity();
+        entity.id = domain.id;
+        entity.name = domain.name;
+        entity.description = domain.description;
+        entity.price = domain.price;
+        entity.stock = domain.stock;
+        entity.category = domain.category;
+        return entity;
+    }
+}
