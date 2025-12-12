2076ac5059241f1e0de76fb27abd194fdfea824c
Angel <apenalver4@gmail.com>
2025-12-11 20:46:09 -0400

    feat(infra): implement advanced search query with fuzzy matching and filters

--- src/products/domain/ports/search-service.port.ts
+++ src/products/domain/ports/search-service.port.ts
@@ -1,5 +1,7 @@
 import { Product } from "../models/product.model";
+import { SearchQuery } from "../value-objects/search-query.vo";
 
 export abstract class SearchServicePort {
+    abstract searchProducts(query: SearchQuery): Promise<Product[]>;
     abstract indexProduct(product: Product): Promise<void>;
-}
+}               
--- src/products/infrastructure/search/elastic-product.adapter.ts
+++ src/products/infrastructure/search/elastic-product.adapter.ts
@@ -1,17 +1,105 @@
-import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
 import { ElasticsearchService } from "@nestjs/elasticsearch";
-import { Product } from "src/products/domain/models/product.model";
-import { SearchServicePort } from "src/products/domain/ports/search-service.port";
+import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
+import { Product } from "../../domain/models/product.model";
+import { SearchQuery } from "../../domain/value-objects/search-query.vo";
+import { SearchServicePort } from "../../domain/ports/search-service.port";
+import { ELASTICSEARCH_CONSTANTS } from "./elasticsearch.constants";
+import {
+    ElasticsearchFilterQuery,
+    ElasticsearchMustQuery,
+} from "./elasticsearch.types";
 
 @Injectable()
 export class ElasticProductAdapter implements SearchServicePort, OnModuleInit {
     private readonly logger = new Logger(ElasticProductAdapter.name);
+
     constructor(private readonly elasticsearchService: ElasticsearchService) { }
 
+    /**
+     * Search products in Elasticsearch
+     * @param query The search query
+     * @returns The search results
+     */
+    async searchProducts(query: SearchQuery): Promise<Product[]> {
+        try {
+            const { q, category, minPrice, maxPrice, offset, limit } = query;
+
+            const must: ElasticsearchMustQuery[] = [];
+            const filters: ElasticsearchFilterQuery[] = [];
+
+            /**
+             * If a query is provided, add it to the must clause
+             * Otherwise, add a match_all query to the must clause
+             */
+            if (q) {
+                must.push({
+                    multi_match: {
+                        query: q,
+                        fields: ['name^2', 'description'],
+                        fuzziness: 'AUTO'
+                    }
+                });
+            } else {
+                must.push({
+                    match_all: {}
+                });
+            }
+
+            /**
+             * If a category is provided, add it to the filter clause
+             */
+            if (category) {
+                filters.push({
+                    term: {
+                        category: category
+                    }
+                });
+            }
+
+            if (minPrice !== undefined || maxPrice !== undefined) {
+                filters.push({
+                    range: {
+                        price: {
+                            ...(minPrice !== undefined && { gte: minPrice }),
+                            ...(maxPrice !== undefined && { lte: maxPrice }),
+                        }
+                    }
+                });
+            }
+
+            this.logger.log('Searching products...');
+            const response = await this.elasticsearchService.search<Product>({
+                index: ELASTICSEARCH_CONSTANTS.PRODUCT_INDEX,
+                from: offset,
+                size: limit,
+                query: {
+                    bool: {
+                        must,
+                        filter: filters
+                    }
+                }
+            });
+
+            this.logger.log(`Products searched by: ${JSON.stringify(query)}`);
+
+            return response.hits.hits.map(hit => hit._source as Product);
+
+        } catch (error) {
+            this.logger.error('Error while searching products:', error.stack);
+            throw error;
+        }
+    }
+
+
+
+    /**
+     * Index a product in Elasticsearch
+     * @param product The product to index
+     */
     async indexProduct(product: Product): Promise<void> {
         try {
             await this.elasticsearchService.index({
-                index: 'products',
+                index: ELASTICSEARCH_CONSTANTS.PRODUCT_INDEX,
                 id: product.id,
                 body: {
                     name: product.name,
@@ -20,7 +108,7 @@ export class ElasticProductAdapter implements SearchServicePort, OnModuleInit {
                     stock: product.stock,
                     category: product.category,
                 }
-            })
+            });
             this.logger.log('Product indexed successfully');
 
         } catch (error) {
@@ -29,19 +117,27 @@ export class ElasticProductAdapter implements SearchServicePort, OnModuleInit {
         }
     }
 
+
+    /**
+     * Initialize the Elasticsearch index if it doesn't exist
+     */
     async onModuleInit() {
         try {
 
             this.logger.log('Initializing Elasticsearch index...');
 
-            const indexExists = await this.elasticsearchService.indices.exists({ index: 'products' });
+            const indexExists = await this.elasticsearchService.indices.exists({
+                index: ELASTICSEARCH_CONSTANTS.PRODUCT_INDEX
+            });
 
             if (!indexExists) {
-                this.logger.log('Index products does not exist, creating...');
-                await this.elasticsearchService.indices.create({ index: 'products' });
-                this.logger.log('Index products created');
+                this.logger.log(`Index ${ELASTICSEARCH_CONSTANTS.PRODUCT_INDEX} does not exist, creating...`);
+                await this.elasticsearchService.indices.create({
+                    index: ELASTICSEARCH_CONSTANTS.PRODUCT_INDEX
+                });
+                this.logger.log(`Index ${ELASTICSEARCH_CONSTANTS.PRODUCT_INDEX} created`);
             } else {
-                this.logger.log('Index products already exists');
+                this.logger.log(`Index ${ELASTICSEARCH_CONSTANTS.PRODUCT_INDEX} already exists`);
             }
 
 
